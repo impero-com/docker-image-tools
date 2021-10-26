d7cd211eb2fa297b43085de8543e996006f2f53e
philipahlberg <philipahlberg@gmail.com>
2021-10-26 11:26:39 +0200

    [sc-17825] Add tools/fetch-cross.sh

---
+++ tools/fetch-cross.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -eu
+
+NAME="cross"
+REPO="rust-embedded"
+VERSION="v0.1.16"
+TARGET="x86_64-unknown-linux-musl"
+FORMAT=".tar.gz"
+
+URL=https://github.com/$REPO/$NAME/releases/download/$VERSION/$NAME-$VERSION-$TARGET$FORMAT
+
+curl --location $URL | tar xz
