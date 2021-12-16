a2b0367e2eae1be37bd85ff23ea519a92fdd1d67
philipahlberg <philipahlberg@gmail.com>
2021-12-16 14:42:35 +0100

    Try aarch64 suffix

--- scripts/docker/install.sh
+++ scripts/docker/install.sh
@@ -20,15 +20,15 @@ case "$1" in
         # Build libssl
         git clone https://github.com/openssl/openssl.git --depth 1
         cd openssl && \
-            ./Configure linux-arm64 --cross-compile-prefix=arm64-linux-gnu- --prefix=/usr/arm64-linux-gnu && \
+            ./Configure linux-arm64 --cross-compile-prefix=aarch64-linux-gnu- --prefix=/usr/aarch64-linux-gnu && \
             make
         cd .. && rm -rf openssl
 
         # Build libpq
         git clone https://github.com/postgres/postgres.git --depth 1
         cd postgres && \
-            CC=arm64-linux-gnu-gcc CXX=arm64-linux-gnu-g++ AR=arm64-linux-gnu-ar RANLIB=arm64-linux-gnu-ranlib; \
-            ./configure --host=arm64-linux-gnu --prefix=/usr/arm64-linux-gnu --without-zlib --without-readline && \
+            CC=aarch64-linux-gnu-gcc CXX=aarch64-linux-gnu-g++ AR=aarch64-linux-gnu-ar RANLIB=aarch64-linux-gnu-ranlib; \
+            ./configure --host=aarch64-linux-gnu --prefix=/usr/aarch64-linux-gnu --without-zlib --without-readline && \
             cd src/interfaces/libpq && make install && \
             cd ../../bin/pg_config && make && make install
 
