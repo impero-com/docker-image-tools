0f6d82b38ffd778a79763457af6fd669a2ba1c14
philipahlberg <philipahlberg@gmail.com>
2021-12-16 15:20:21 +0100

    Fix argument reference in export.sh

--- scripts/docker/export.sh
+++ scripts/docker/export.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -eu
 
-mkdir -p $2
-cp $CARGO_HOME/bin/sccache $2/
-cp $CARGO_HOME/bin/diesel $2/
-cp $CARGO_HOME/bin/diesel_ext $2/
-cp $CARGO_HOME/bin/cargo-audit $2/
-cp $CARGO_HOME/bin/cargo-watch $2/
+mkdir -p $1
+cp $CARGO_HOME/bin/sccache $1/
+cp $CARGO_HOME/bin/diesel $1/
+cp $CARGO_HOME/bin/diesel_ext $1/
+cp $CARGO_HOME/bin/cargo-audit $1/
+cp $CARGO_HOME/bin/cargo-watch $1/
