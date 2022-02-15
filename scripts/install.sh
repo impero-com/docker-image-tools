4137183a6201888836e3e86ce71de969924ba9b1
philipahlberg <philipahlberg@gmail.com>
2022-02-15 09:25:13 +0100

    Try another PG tag

--- scripts/install.sh
+++ scripts/install.sh
@@ -18,7 +18,7 @@ cd openssl && \
 cd .. && rm -rf openssl
 
 # Build libpq
-git clone https://github.com/postgres/postgres.git --depth 1 --branch REL_11_15
+git clone https://github.com/postgres/postgres.git --depth 1 --branch REL_14_2
 cd postgres && \
     CC=aarch64-linux-gnu-gcc CXX=aarch64-linux-gnu-g++ AR=aarch64-linux-gnu-ar RANLIB=aarch64-linux-gnu-ranlib; \
     ./configure --host=aarch64-linux-gnu --prefix=/usr/aarch64-linux-gnu --without-zlib --without-readline && \
