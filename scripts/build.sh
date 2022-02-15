e3d8df083e7b294b1ebc1235c913c39fd487440e
philipahlberg <philipahlberg@gmail.com>
2022-02-15 13:11:47 +0100

    Fix x86_64 builder image

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,11 +2,17 @@
 set -eu
 
 cargo install sccache --version 0.2.15 --no-default-features --target "$1"
+echo "\n"
 cargo install diesel_cli --version 1.4.1 --no-default-features --features postgres --target "$1"
+echo "\n"
 cargo install diesel_cli_ext --version 0.3.6 --target "$1"
+echo "\n"
 cargo install cargo-audit --version 0.15.0 --features vendored-openssl --target "$1"
+echo "\n"
 cargo install cargo-watch --version 8.1.1 --target "$1"
+echo "\n"
 
+echo "Copying files to /export/$1"
 mkdir -p /export/$1
 cp $CARGO_HOME/bin/sccache /export/$1/
 cp $CARGO_HOME/bin/diesel /export/$1/
