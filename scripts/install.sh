de95a43a3885a7eef2e9c994dad644fca2c06586
philipahlberg <philipahlberg@gmail.com>
2022-04-19 16:12:32 +0200

    Add hi-tech println debugging

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,12 +11,20 @@ export PKG_CONFIG_aarch64_unknown_linux_gnu=/usr/bin/aarch64-linux-gnu-pkg-confi
 export PKG_CONFIG_PATH_aarch64_unknown_linux_gnu=/usr/bin/aarch64-linux-gnu-pkg-config
 export PKG_CONFIG_SYSROOT_DIR=/usr/aarch64-linux-gnu
 
+echo "\n"
+ls /usr/aarch64-linux-gnu
+echo "\n"
+
 # Build libssl
 git clone https://github.com/openssl/openssl.git --depth 1 --branch OpenSSL_1_1_1m
 cd openssl && \
     ./Configure linux-arm64 --cross-compile-prefix=aarch64-linux-gnu- --prefix=/usr/aarch64-linux-gnu && \
     make
 cd .. && rm -rf openssl
+
+echo "\n"
+ls /usr/aarch64-linux-gnu
+echo "\n"
 
 # Build libpq
 git clone https://github.com/postgres/postgres.git --depth 1 --branch REL_14_2
@@ -25,6 +33,10 @@ cd postgres && \
     ./configure --host=aarch64-linux-gnu --prefix=/usr/aarch64-linux-gnu --without-zlib --without-readline && \
     cd src/interfaces/libpq && make install && \
     cd ../../bin/pg_config && make && make install
+
+echo "\n"
+ls /usr/aarch64-linux-gnu
+echo "\n"
 
 cd .. && rm -rf postgres
 
